f358eaeefe2a98328216452b40bab837b51de606
Pal2456 <6522772456@g.siit.tu.ac.th>
2024-11-25 16:35:45 +0700

    add image

--- app/routes/index.js
+++ app/routes/index.js
@@ -16,8 +16,8 @@ let secretCode = process.env.SECRET_CODE;
 let dayFormat = process.env.DAY_FORMAT;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 // ใช้ dotenv เพื่อเรียกใช้งานไฟล์ .env ที่เก็บข้อมูลที่เป็นค่า const ที่ไม่ควรเปิดเผย
-
-
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
